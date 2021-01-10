b0c96e7405fb22fc253b5a9371fe7c40f2dffd7b
Christopher Guess <cguess@gmail.com>
2021-01-10 16:33:21 -0500

    trying to get it running

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ require('@google-cloud/debug-agent').start({serviceContext: {enableCanary: true}
 const escapeHtml = require('escape-html');
 const bucketName = 'wiscocovidtest'
 
-exports.listLogFiles = () => {
-  // Lists files in the bucket
-  const [files] = storage.bucket(bucketName).getFiles();
-
-  console.log('Files:');
-  files.forEach(file => {
-    console.log(file.name);
-  });
-}
-
 exports.loadLogs = (request, response) => {
+  // This is oddly structured, but I can't figure out how to actually use more than one function.
+
+  listLogFiles = () => {
+    // Lists files in the bucket
+    const [files] = storage.bucket(bucketName).getFiles();
+
+    console.log('Files:');
+    files.forEach(file => {
+      console.log(file.name);
+    });
+  }
+
+
   listFiles().catch(console.error);
 };
 
