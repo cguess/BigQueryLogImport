5aa91b3d13f80f33393c586c3ee33f2bb2aea54e
Christopher Guess <cguess@gmail.com>
2021-01-10 15:34:51 -0500

    Fix entry function

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const escapeHtml = require('escape-html');
  * @param {Object} res Cloud Function response context.
  *                     More info: https://expressjs.com/en/api.html#res
  */
-exports.helloHttp = (req, res) => {
+exports.loadLogs = (req, res) => {
   res.send(`Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);
 };
