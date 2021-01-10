a5189d5ce8b5faef002b2c27fb2248052a858c3f
Christopher Guess <cguess@gmail.com>
2021-01-10 16:55:00 -0500

    probably got listing working

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('@google-cloud/debug-agent').start({serviceContext: {enableCanary: true}});
 
 const {Storage} = require('@google-cloud/storage');
-const storage = new Storage();
 
 const bucketName = 'wiscocovidtest'
 
 function listLogFiles() {
   // Lists files in the bucket
+  const storage = new Storage();
+  console.log(`storage: ${storage}`)
   const [files] = storage.bucket(bucketName).getFiles();
 
   console.log('Files:');
